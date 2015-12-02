a91721160a0e5be1b9bf250745784333272f754f
Andrew Stocker <amstocker@dons.usfca.edu>
2015-12-02 15:03:43 -0800

    init

---
+++ day2/day2.py
@@ -0,0 +1,34 @@
+"""
+Advent of Code: Day 2
+"""
+from operator import mul
+from itertools import combinations
+
+
+# input data
+if __name__ == "__main__":
+    with open('input.txt') as f:
+        sizes = [map(int, t.split('x')) for t in f.readlines()]
+
+
+"""
+Part 1
+"""
+def wrapping_needed(dims):
+    areas = [reduce(mul, c, 1) for c in combinations(dims, 2)]
+    return 2*sum(areas) + min(areas)
+
+if __name__ == "__main__":
+    print sum(map(wrapping_needed, sizes))
+
+
+"""
+Part 2
+"""
+def ribbon_needed(dims):
+    ribbon = 2*sum(sorted(dims)[:2])
+    bow = reduce(mul, dims, 1)
+    return ribbon + bow
+
+if __name__ == "__main__":
+    print sum(map(ribbon_needed, sizes))
