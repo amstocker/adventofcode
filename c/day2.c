bfbde93a81e2540e5489dfc852a36683e21255a2
Andrew Stocker <amstocker@dons.usfca.edu>
2015-12-21 19:38:29 -0800

    drastically better day2 in C

--- c/day2.c
+++ c/day2.c
@@ -1,82 +1,35 @@
 #include "aoc.h"
 
-#define BUF_SIZE 256
 
-
-int *make_3tuples(char* in, int len) {
-    
-    int *tuples = malloc(sizeof(int) * 3 * len);
-    int *ptr = tuples;
-    
-    char *start;
-    char buf[BUF_SIZE];
-    while (*in) {
-        start = in;
-        while(*in != '\n' && *in != '\0') {
-            in++;
-        }
-        if ((int)(in-start) > (BUF_SIZE-1)) {
-            error("max line length exceeded");
-        }
-        
-        memset(buf, 0, sizeof(buf));
-        memcpy(buf, start, (int)(in-start));
-        buf[BUF_SIZE - 1] = '\0';
-        
-        if (sscanf(buf, "%dx%dx%d", ptr, ptr+1, ptr+2) == EOF) {
-            error("error scanning list of package sizes");
-        }
-        ptr += 3;
-        
-        if (*in == '\n') {
-            in++;
-        }
-    }
-    return tuples;
-}
-
-
-int wrapping_needed(int l, int w, int h) {
-    int areas[] = { l*w, w*h, h*l };
+int wrapping_needed(int *arr) {
+    int areas[] = { arr[0] * arr[1],
+                    arr[1] * arr[2],
+                    arr[2] * arr[0],
+                    };
     return 2 * int_array_sum(areas, 3) + int_array_min(areas, 3);
 }
 
 
-int ribbon_needed(int *array) {
-    int *mins = int_array_mins(array, 3, 2);
-    return 2 * int_array_sum(mins, 2) + int_array_prod(array, 3);
+int ribbon_needed(int *arr) {
+    int *mins = int_array_mins(arr, 3, 2);
+    return 2 * int_array_sum(mins, 2) + int_array_prod(arr, 3);
 }
 
 
-void part1(int* tuples, int len) {
-    int sum = 0;
-    for (int i = 0; i < len; i++) {
-        sum += wrapping_needed(tuples[0], tuples[1], tuples[2]);
-        tuples += 3;
+int main() {
+    FILE *fp = fopen("day2_input.txt", "r");
+
+    int wrapping_sum = 0;
+    int ribbon_sum = 0;
+
+    int t[3];
+    while (fscanf(fp, "%dx%dx%d", &t[0], &t[1], &t[2]) != EOF) {
+        wrapping_sum += wrapping_needed(t);
+        ribbon_sum += ribbon_needed(t);
     }
-    printf("part I sum: %d\n", sum);
-}
 
+    printf("wrapping: %d\n", wrapping_sum);
+    printf("ribbon: %d\n", ribbon_sum);
 
-void part2(int* tuples, int len) {
-    int sum = 0;
-    for (int i = 0; i < len; i++) {
-        sum += ribbon_needed(tuples);
-        tuples += 3;
-    }
-    printf("part II sum: %d\n", sum);
-}
-
-
-int main(int argc, char *argv[argc+1]) {
-    char *in = input_open("day2_input.txt");
-    int nlines = str_count_lines(in);
-    int *tuples = make_3tuples(in, nlines);
-
-    part1(tuples, nlines);
-    part2(tuples, nlines);
-
-    free(in);
-    free(tuples);
     return 0;
 }
