b2e02d7ecb2d68b9596241f0074b2f9cb11aabad
Andrew Stocker <amstocker@dons.usfca.edu>
2015-12-21 23:31:38 -0800

    minor

--- c/aoc.h
+++ c/aoc.h
@@ -13,13 +13,15 @@
 // error.c
 void error(const char *msg);
 
+
 // utils.c
 int int_array_min(const int *array, const int len);
 int *int_array_mins(const int *array, const int len, const int n);
 int int_array_sum(const int *array, const int len);
 int int_array_prod(const int *array, const int len);
 
-// quadtree.c
+
+// qtree.c
 typedef struct QTNode {
     int x;
     int y;
